67b9979d48687315d8b410cda6ac024c7f9d12a7
lgirardi94 <lgirardi94@gmail.com>
2026-04-24 14:37:10 +0200

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,11 +1,7 @@
 export class Store {
   constructor() {
     this.key = 'apiario-data';
-    this.data = this.load();
-  }
-
-  load() {
-    return JSON.parse(localStorage.getItem(this.key)) || {
+    this.data = JSON.parse(localStorage.getItem(this.key)) || {
       arnie: [],
       log: [],
       magazzino: [],
