b6a382449a3d75816e25d4925b9e7161c50e6f6d
lgirardi94 <lgirardi94@gmail.com>
2026-04-24 10:36:00 +0200

    Create arnie.js

---
+++ arnie.js
@@ -0,0 +1,13 @@
+import { uuid } from '../utils.js';
+
+export function addArnia(store, nome) {
+  store.data.arnie.push({
+    id: uuid(),
+    nome
+  });
+  store.save();
+}
+
+export function getArnie(store) {
+  return store.data.arnie;
+}
