aa1b9055f0d84ccffd8574a615c527e2797019ae
Lu94 <lgirardi94@gmail.com>
2026-05-26 14:05:27 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,4 @@
-
 // ===== NAVIGAZIONE =====
-
 function navigateTo(id) {
   const btn = [...document.querySelectorAll('nav button')].find(b => b.getAttribute('onclick')?.includes(`'${id}'`));
   document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
